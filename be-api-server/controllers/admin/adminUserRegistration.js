7c53ed93d4c90077081eb1c28738e47af424d785
manassantra <manassantra.contact@gmail.com>
2025-06-24 16:17:38 +0530

    modified user registration & sigin function created

--- be-api-server/controllers/admin/adminUserRegistration.js
+++ be-api-server/controllers/admin/adminUserRegistration.js
@@ -1,48 +1,75 @@
-const bcrypt = require('bcrypt'),
-      crypto = require('crypto'),
-      AdminUser = require('../../models/adminUser');
+const bcrypt = require('bcrypt');
+const crypto = require('crypto');
+const AdminUser = require('../../models/adminUser');
 
+const adminUserRegistration = async (req, res) => {
+    const { email, mob, fullName, gender, password } = req.body;
 
-const adminUserRegistration = async(req, res, next) => {
-    if (req.body.email && req.body.mob) {
-        if (req.body.fullName && req.body.gender && req.body.password) {
-            const user = await ifUserExist(req.body.email?.trim(), req.body.mob?.toString().trim());
-            if (!user) {
-                let passwordSalt = bcrypt.genSaltSync(10);
-                let passwordHash = bcrypt.hashSync(req.body.password, passwordSalt);
-                let newuser = new AdminUser({
-                    id: crypto.randomBytes(16).toString("hex"),
-                    fullName: req.body.fullName,
-                    email: req.body.email,
-                    mob: req.body.mob,
-                    gender: req.body.gender,
-                    password: passwordHash
-                });
-                const result = await newuser.save();
-                if (!result) {
-                    res.send({status: "Error", status_code: 409, message: "There is an error! \nTry again later!"});
-                } else {
-                    res.send({status: "Success", status_code: 200, message: "User created successfully"});
-                }
-            } else {
-                res.send({status: "Error", status_code: 409, message: "User alreay exist! \nTry another email & mobile!"});
-            }
-        } else {
-            res.send({status: "Error", status_code: 409, message: "Full-Name / Gender / Password is missing !"});
-        }
-    } else {
-        res.send({status: "Warning", status_code: 404, message: "Email/Mobile is missing !"});
+    if (!email || !mob) {
+        return res.status(400).send({
+            status: "Warning",
+            message: "Email/Mobile is missing!"
+        });
     }
-}
+
+    if (!fullName || !gender || !password) {
+        return res.status(400).send({
+            status: "Error",
+            message: "Full-Name / Gender / Password is missing!"
+        });
+    }
+
+    const userExists = await ifUserExist(email.trim(), mob.toString().trim());
+
+    if (userExists) {
+        return res.status(409).send({
+            status: "Error",
+            message: "User already exists! \nTry another email & mobile!"
+        });
+    }
+
+    try {
+        const passwordSalt = bcrypt.genSaltSync(10);
+        const passwordHash = bcrypt.hashSync(password, passwordSalt);
+
+        const newUser = new AdminUser({
+            id: crypto.randomBytes(16).toString("hex"),
+            fullName,
+            email,
+            mob,
+            gender,
+            password: passwordHash
+        });
+
+        const result = await newUser.save();
+
+        if (!result) {
+            return res.status(500).send({
+                status: "Error",
+                message: "There was an error! \nTry again later!"
+            });
+        }
+
+        return res.status(201).send({
+            status: "Success",
+            message: "User created successfully"
+        });
+
+    } catch (error) {
+        return res.status(500).send({
+            status: "Error",
+            message: "Server error. Please try again later."
+        });
+    }
+};
 
 async function ifUserExist(email, mob) {
-  const user = await AdminUser.findOne({
-    $or: [{ email }, { mob }]
-  });
-  return !!user;
+    const user = await AdminUser.findOne({
+        $or: [{ email }, { mob }]
+    });
+    return !!user;
 }
-
 
 module.exports = {
     adminUserRegistration
-}
+};
