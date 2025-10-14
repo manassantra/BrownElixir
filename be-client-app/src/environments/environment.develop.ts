bf4d7fe1f1889f1ca482f040d2f9e5f996f37547
Manas Santra <manas.nshmdgp@gmail.com>
2025-10-14 23:47:14 +0530

    updated env

--- be-client-app/src/environments/environment.develop.ts
+++ be-client-app/src/environments/environment.develop.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    BASE_API: "https://chocobitez-api-server.onrender.com/api",
+    BASE_API: "http://localhost:3000/api",
     API_KEY: "9f2e7d5428d6a9fc8d2c7183b2c0e007",
     API_SECRET: "0f7a89e264cf0dc5a4a3964cbd9d39f231b7f3c6b67c58e8f6a7856b369a5c72",
     CART_KEY: "_cHoCoBitz_cartItem_5a4a3964cbd9d_"
