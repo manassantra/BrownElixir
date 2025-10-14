dd90af2ed447e158535eff1ad16f50b28a5a04c7
Manas Santra <manas.nshmdgp@gmail.com>
2025-10-14 22:31:48 +0530

    patch v2.5

--- be-client-app/src/app/services/address-service.ts
+++ be-client-app/src/app/services/address-service.ts
@@ -1,5 +1,5 @@
 import { DOCUMENT, Inject, Injectable } from '@angular/core';
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../environments/environment.dev';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
